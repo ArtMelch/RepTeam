0cdac1e68bf30c8e638df83daa01f3d23035b009
ArtMelch <elfalsoyoponce@gmail.com>
2024-09-07 20:31:30 -0600

    Ya termine el proyecto

--- Meta.py
+++ Meta.py
@@ -3,6 +3,7 @@ for i in range (9, 40):
     print("Joshua Nataniel Espinobarros Rodriguez")
     print(i)
 
-
-
-
+    if (i == 10) :
+        print("Vamos en el numero 10")
+    else:
+        pass
